d4fec99a375b9a313fb445d6fd1ad411d7ff97dc
Ryry <45459303+RyanTF313@users.noreply.github.com>
2023-05-17 22:45:32 -0400

    Update LongestCommonPrefix.js

--- LongestCommonPrefix.js
+++ LongestCommonPrefix.js
@@ -30,3 +30,15 @@ var longestCommonPrefix = function(strs) {
   // prefix is the substring from the beginning to the last successfully checked i
   return strs[0].substr(0, i);
 };
+
+// SOLUTION 2
+var longestCommonPrefix = function(strs) {
+    'use strict';
+    if (strs === undefined || strs.length === 0) { return ''; }
+    
+    return strs.reduce((prev, next) => {
+        let i = 0;
+        while (prev[i] && next[i] && prev[i] === next[i]) i++;
+        return prev.slice(0, i);
+    });
+};
